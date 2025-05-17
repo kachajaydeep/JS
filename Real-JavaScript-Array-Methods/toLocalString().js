97e9eda6be65956da30203bad358270e5a275066
Jaydeep Kacha <jaydeepkacha8903@gmail.com>
2025-05-17 11:30:10 +0530

    Array Methods Sort, map().

---
+++ Real-JavaScript-Array-Methods/toLocalString().js
@@ -0,0 +1,14 @@
+let name = "geeksforgeeks";
+let number = 567;
+
+// It will give current date and time.
+let date = new Date();
+
+// Here A is an array containing elements of above variables.
+let A = [name, number, date];
+
+// applying array.toLocaleString function.
+let string = A.toLocaleString();
+
+// printing string.
+console.log(string);
